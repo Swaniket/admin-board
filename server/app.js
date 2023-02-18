25b14c011fdeb856dee73c7c6199a2c6270d6ae9
Swaniket <swaniketchowdhury@gmail.com>
2023-02-18 18:53:34 +0530

    Initialized server

---
+++ server/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/clientRoutes.js";
+import generalRoutes from "./routes/generalRoutes.js";
+import managementRoutes from "./routes/managementRoutes.js";
+import salesRoutes from "./routes/salesRoutes.js";
+
+/* CONFIG */
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+/* 
+ROUTE DESCRIPTION - 
+    /client: Represents all the API required for Client Facing Pages.
+    /sales: Represents all the API required for Sales Page Data.
+    /management: Represents all the API required for the Management Section.
+    /general: Represents all other APIs, like Users, Authentication.
+*/
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
